d253c9f1129bc4de1062191b9461233efb468c93
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2025-04-08 18:12:49 +0200

    Routing in Angular

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ee00b02bb5e7ccf9b4774099f73145e16874a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd5c9888f7a961ae6794cea8ce950a4aefa74bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
