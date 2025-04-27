cb43c1259f9e72b505eda34de5b13eacba8c3d05
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2025-04-27 10:13:22 +0200

    Error handling

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd5c9888f7a961ae6794cea8ce950a4aefa74bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d253c9f1129bc4de1062191b9461233efb468c93")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
