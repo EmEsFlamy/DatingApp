c8f241b85ee7ebf0708d911bdc11c36fc8828307
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2025-05-19 08:46:07 +0200

    User interface

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb43c1259f9e72b505eda34de5b13eacba8c3d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd1c05b2683d22de2584e5536281cb6b2b6dc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
